a754e5aa1f566641efacc55e30ee6a9b83be4c68
Eike Spielberg <Eike.Spielberg@uni-due.de>
2018-12-19 10:28:40 +0100

    inlcusion of reports module

--- src/main/java/unidue/ub/services/mediafrontend/MediaFrontendApplication.java
+++ src/main/java/unidue/ub/services/mediafrontend/MediaFrontendApplication.java
@@ -2,18 +2,16 @@ package unidue.ub.services.mediafrontend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.SecurityProperties;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 import org.springframework.stereotype.Controller;
 
 @Controller
 @SpringBootApplication
 @EnableEurekaClient
-@Order(SecurityProperties.ACCESS_OVERRIDE_ORDER)
 public class MediaFrontendApplication {
 
 	public static void main(String[] args) {
@@ -21,15 +19,13 @@ public class MediaFrontendApplication {
 	}
 
 	@Configuration
-	@Order(SecurityProperties.ACCESS_OVERRIDE_ORDER)
 	protected static class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
 		@Override
 		protected void configure(HttpSecurity http) throws Exception {
-			http.httpBasic()
-					.and()
-					.authorizeRequests().anyRequest().authenticated();
-			http.csrf().disable();
+			http.httpBasic().disable();
+			http.authorizeRequests().anyRequest().authenticated();
+			http.csrf().csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse());
 		}
 	}
 }
